d8f83d0c4a35da5eb0f7d08a0bba9a90c035f30c
박수진 <sherry5667@naver.com>
2025-09-16 19:12:16 +0900

    2

---
+++ l.py
@@ -0,0 +1,17 @@
+import nltk
+
+##nltk.app.chartparser_app.app()	
+
+## nltk.app.chunkparser_app.app()	
+
+## nltk.app.collocations_app.app()	 
+
+#nltk.app.concordance_app.app()	
+
+## nltk.app.nemo_app.app()	
+
+# nltk.app.rdparser_app.app()	
+
+nltk.app.srparser_app.app()	
+
+##nltk.app.wordnet_app.app()
